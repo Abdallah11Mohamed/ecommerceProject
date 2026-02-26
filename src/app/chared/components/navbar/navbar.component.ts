1a2020b28923a7a14a487b1d4f0d5097cab485d6
Abdallah elgohary <leaderabdallah20@gmail.com>
2026-02-26 23:37:32 +0200

    Update navbar and login UI

--- src/app/chared/components/navbar/navbar.component.ts
+++ src/app/chared/components/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
+import { FlowbiteService } from './../../../core/services/flowbite/flowbite.service';
 import { Component, inject, Input } from '@angular/core';
 
 import { RouterLink, RouterLinkActive } from "@angular/router";
 import { AuthService } from '../../../core/auth/services/authantication/auth.service';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-navbar',
@@ -10,8 +12,18 @@ import { AuthService } from '../../../core/auth/services/authantication/auth.ser
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-private readonly authService = inject(AuthService)
- @Input({required:true}) isLogin!:boolean;
+
+ private readonly flowbiteService = inject(FlowbiteService)
+ private readonly authService = inject(AuthService)
+ 
+  @Input({required:true}) isLogin!:boolean;
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite) => {
+      initFlowbite();
+    });
+  }
+
 
 
 
